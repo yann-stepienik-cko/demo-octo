78a5edb62b13989e66768194bee81700b58819bf
Yann Stepienik <yann.stepienik@checkout.com>
2016-11-07 11:26:49 +0000

    circleci

--- gulpfile.js
+++ gulpfile.js
@@ -1,17 +1,20 @@
-
+var gulp = require('gulp');
 var octo = require('@octopusdeploy/gulp-octo');
 require('gulp-release-it')(gulp);
+var runSequence = require('run-sequence');
+var execSync = require('child_process').execSync;
 
-gulp.task("bump-rc", function() {
-  execSync("npm run bump -- prerelease");
-});
 
-gulp.task("bump-release", function() {
-  execSync("npm run bump -- release");
+gulp.task("bump", function() {
+  execSync("npm run bump");
 });
 
 gulp.task("octopus", function() {
   return gulp.src("index.js")
     .pipe(octo.pack('zip'))
     .pipe(octo.push({host: 'http://devapi.checkout.com:8080/', apiKey: 'API-DHBUQTKYBMZ1P0FRIJZIKJMFSI'}));
+});
+
+gulp.task("release", function(done) {
+   runSequence("bump", "octopus", done);
 });
